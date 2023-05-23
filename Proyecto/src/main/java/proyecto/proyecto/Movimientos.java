65ca207a8c0bfe935e664dd53be99b386e1a904b
DAM129 <angel.garciagarcia>
2023-05-23 19:57:23 +0200

    commit uno programacion

---
+++ Proyecto/src/main/java/proyecto/proyecto/Movimientos.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.proyecto;
+
+/**
+ *
+ * @author DAM129
+ */
+public class Movimientos implements Identificable{
+    private int idOperacion;
+    private TipoOperacion tipoOperacion;
+    private double cantidad;
+    private String concepto;
+    private String emisor;
+    private String receptor;
+
+    public int getIdOperacion() {
+        return idOperacion;
+    }
+
+    public void setIdOperacion(int idOperacion) {
+        this.idOperacion = idOperacion;
+    }
+
+    public TipoOperacion getTipoOperacion() {
+        return tipoOperacion;
+    }
+
+    public void setTipoOperacion(TipoOperacion tipoOperacion) {
+        this.tipoOperacion = tipoOperacion;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getEmisor() {
+        return emisor;
+    }
+
+    public void setEmisor(String emisor) {
+        this.emisor = emisor;
+    }
+
+    public String getReceptor() {
+        return receptor;
+    }
+
+    public void setReceptor(String receptor) {
+        this.receptor = receptor;
+    }
+    
+    
+}
