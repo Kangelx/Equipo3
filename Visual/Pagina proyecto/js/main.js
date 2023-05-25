3a9bac6f604a118b718a69f2cb2febcf5db5e9c1
Adrian de Diego <adrian.dediegofernandez@iesmiguelherrero.com>
2023-05-25 20:05:13 +0200

    pagina xhtml

--- Visual/Pagina proyecto/js/main.js
+++ Visual/Pagina proyecto/js/main.js
@@ -34,14 +34,14 @@ $(document).ready(function(){
 	$('.btn-exit-system').on('click', function(e){
 		e.preventDefault();
 		Swal.fire({
-			title: 'Are you sure to close the session?',
-			text: "You are about to close the session and exit the system",
+			title: '¿Está seguro de cerrar la sesión?',
+			text: "Está a punto de cerrar la sesión y salir del sistema",
 			type: 'question',
 			showCancelButton: true,
 			confirmButtonColor: '#3085d6',
 			cancelButtonColor: '#d33',
-			confirmButtonText: 'Yes, exit!',
-			cancelButtonText: 'No, cancel'
+			confirmButtonText: 'Si, salir!',
+			cancelButtonText: 'No, cancelar'
 		}).then((result) => {
 			if (result.value) {
 				window.location="index.html";
